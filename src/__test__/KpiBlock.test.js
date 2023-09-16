1020be808717d689bf1df9991c6807e1928ece76
Hanieh <mohseni.hanieh@gmail.com>
2023-09-16 15:47:53 -0400

    Add KpiBlock.test

---
+++ src/__test__/KpiBlock.test.js
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+import { expect, test } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import KpiBlock from "../components/base/KpiBlock";
+
+test("displays positive color of value to user when greater than target value", async () => {
+  //   ARRANGE
+  render(
+    <KpiBlock
+      svgComponent={null}
+      title="conversion"
+      time="today"
+      value="19%"
+      targetValue={4}
+    />
+  );
+
+  // ACT
+  let displayedValue = await screen.getByText("19%");
+
+  //   ASSERT
+  expect(displayedValue.className).toContain("text-blue-900");
+});
